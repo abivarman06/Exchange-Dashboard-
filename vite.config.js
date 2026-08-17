c69ae4186ee2c4fe0c7cf47dde79d5e0f8f5bcb9
abivarman06 <159875165+abivarman06@users.noreply.github.com>
2026-08-17 22:45:08 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// IMPORTANT: base must match your repo name for GitHub Pages, e.g. "/exchange-ledger/"
-export default defineConfig({
-  plugins: [react()],
-  base: "/exchange-ledger/",
-});
