4f8f00fc1ff7b74cd1984ac6c8619025f6f14cfb
abivarman06 <159875165+abivarman06@users.noreply.github.com>
2026-08-17 22:46:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // IMPORTANT: base must match your repo name for GitHub Pages, e.g. "/exchange-ledger/"
 export default defineConfig({
   plugins: [react()],
-  base: "/exchange-ledger/",
+base: "/Exchange-Dashboard-/",
 });
